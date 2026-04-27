f29f3640fb59aa86c769220eb6e175bcf3c8786e
Mohammed Rabil <mail@rabil.me>
2026-04-28 00:05:40 +0530

    Remove version declaration from __init__.py
    Removed version declaration from __init__.py.

--- src/music_liked_sync/__init__.py
+++ src/music_liked_sync/__init__.py
@@ -1,7 +1,5 @@
 """Bidirectional sync for Spotify and YouTube Music liked songs."""
 
-__version__ = "0.1.1"
-
 from .models import Track as Track
 from .spotify import SpotifyBackend as SpotifyBackend
 from .ytmusic import YTMusicBackend as YTMusicBackend
